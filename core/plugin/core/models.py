8bf9085b8b90eaa48bb186c9faa0923e8f4a837b
savic-a <anastasijas557@gmail.com>
2022-12-27 18:05:37 +0100

    #12 Created vertex class

--- core/plugin/core/models.py
+++ core/plugin/core/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+class Vertex:
+    __slots__ = '_attributes', '_id', '_edges'
+
+    def __init__(self, id):
+        self._attributes = {}
+        self._id = id
+        self._edges = []
+
+    @property
+    def attributes(self):
+        return self._attributes
+
+    @attributes.setter
+    def attributes(self, attributes):
+        self._attributes = attributes
+
+    @property
+    def id(self):
+        return self._id
+
+    @id.setter
+    def id(self, id):
+        self._id = id
+
+    @property
+    def edges(self):
+        return self._edges
+
+    @edges.setter
+    def edges(self, edges):
+        self._edges = edges
+
+    def add_attribute(self, key, value):
+        self._attributes[key] = value
+
+    def add_edge(self, edge):
+        self._edges.append(edge)
+
+    def __hash__(self):
+        return hash(self._id)

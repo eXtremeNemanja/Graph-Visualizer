b4709381f25f07141937ea8902b001ff91d18b9b
Nemanja Dutina <dutina.sv.27@uns.ac.rs>
2023-01-17 20:55:02 +0100

    #20 Created function to convert xml tree into graph

--- xml_loader/plugin/loader/xml_loader.py
+++ xml_loader/plugin/loader/xml_loader.py
@@ -17,5 +17,21 @@ class XmlLoader(BaseLoader):
         root = tree.getroot()
         return root
 
-    def make_graph(self, tree):
-        return super().make_graph(tree)
+    def create_vertex(graph, node, id_counter):
+        id_counter
+        v = Vertex(id_counter)
+        for key in node.attrib:
+            v.add_attribute(key, node.attrib.get(key))
+        if node.text != "":
+            v.add_attribute("text", node.text)
+        v.add_attribute()
+        graph.insert_vertex(v)
+        for child in node:
+            c = create_vertex(graph, child, id_counter)
+            e = Edge(v, c, c.tag, 0, True)
+            v.add_edge(e)
+        return v
+
+    def make_graph(self, tree, graph_name):
+        g = Graph(graph_name)
+        create_vertex(g, tree, 0)

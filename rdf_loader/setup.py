bc6906524fba42109869543c5065acf0d02ad31f
Milica Sladaković <milicasladakovic7@gmail.com>
2023-01-31 00:13:18 +0100

    #30 Created package

--- rdf_loader/setup.py
+++ rdf_loader/setup.py
@@ -1,16 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="xml-loader",
-    author="Nemanja Dutina",
+    name="rdf-loader",
+    author="Katarina Vučić",
     version="0.1",
     packages=find_packages(),
     namespace_packages=['plugin'],
     # TODO add entry_points
-    entry_points={
-        'loader':
-        ['xml-loader=plugin.loader.xml_loader:XmlLoader']
-    },
     install_requires=["core>=0.1"],
     zip_safe=True
 )

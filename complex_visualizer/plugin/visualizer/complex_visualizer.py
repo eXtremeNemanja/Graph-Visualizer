835fd7b3342570489a0f406c55c7ea4c54e0d795
savic-a <anastasijas557@gmail.com>
2023-02-01 18:01:01 +0100

    #39 Added pan and zoom

--- complex_visualizer/plugin/visualizer/complex_visualizer.py
+++ complex_visualizer/plugin/visualizer/complex_visualizer.py
@@ -35,7 +35,6 @@ class ComplexVisualizer(BaseVisualizer):
                     .node {
                     cursor: pointer;
                     color: #003B73;
-
                     }
 
                     .link {
@@ -45,9 +44,7 @@ class ComplexVisualizer(BaseVisualizer):
                     }
 
                     </style>
-                        <svg id="mainView" width="100%" height="100%">
-
-                        </svg>
+                        <svg id="mainView" width="100%" height="100%"></svg>
                     <script>
 
                     var nodesGraph = JSON.parse("{{nodes |escapejs}}");                
@@ -63,15 +60,19 @@ class ComplexVisualizer(BaseVisualizer):
                         }
 
                         var force = d3.layout.force() //kreiranje force layout-a
-                            .size([400, 400]) //raspoloziv prostor za iscrtavanje
+                            .size([800, 650]) //raspoloziv prostor za iscrtavanje
                             .nodes(d3.values(nodesGraph)) //dodaj nodove
                             .links(linksGraph) //dodaj linkove
                             .on("tick", tick) //sta treba da se desi kada su izracunate nove pozicija elemenata
-                            .linkDistance(300) //razmak izmedju elemenata
-                            .charge(-100)//koliko da se elementi odbijaju
+                            .linkDistance(125) //razmak izmedju elemenata
+                            .charge(-2000)//koliko da se elementi odbijaju
                             .start(); //pokreni izracunavanje pozicija
 
-                        var svg=d3.select('#mainView');
+                        // add pan and zoom
+                        var svg = d3.select('#mainView').call(d3.behavior.zoom().on("zoom", function () {
+                                svg.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")")
+                        })).append('g');
+
                         // add the links
                         var link = svg.selectAll('.link')
                             .data(linksGraph)
@@ -87,50 +88,48 @@ class ComplexVisualizer(BaseVisualizer):
                             .on('click',function(){
                             nodeClick(this);
                             });
-                        d3.selectAll('.node').each(function(d){slozenPrikaz(d);});
+                        d3.selectAll('.node').each(function(d){complexView(d);});
 
-                        function slozenPrikaz(d){
-                        var length = -Infinity;
-                        for(var i=0;i<d.attributes.length;i++){
-                            if(length<d.attributes[i].length) length = d.attributes[i].length
-                        }
+                        function complexView(d) {
+                            var length = -Infinity;
+                            for(var i=0;i<d.attributes.length;i++) {
+                                if(length<d.attributes[i].length) length = d.attributes[i].length
+                            }
 
-                        var attributesNum = d.attributes.length;
+                            var attributesNum = d.attributes.length;
 
-                        var textSize = 12;
-                        var visina = 30;
-                        visina += (attributesNum == 0) ? textSize: attributesNum*textSize;
-                        var duzina = length * textSize/2;
+                            var textSize = 12;
+                            var high = 30;
+                            high += (attributesNum == 0) ? textSize: attributesNum*textSize;
+                            var width = length * textSize/2;
 
-                        //Ubacivanje kvadrata
-                        d3.select("g#"+d.id).append('rect').
-                            attr('x',0).attr('y',0).attr('width',duzina).attr('height',visina)
-                            .attr('fill','#003B73');
-                        //Ubacivanje naziva prodavnice ili artikla
-                        d3.select("g#"+d.id).append('text').attr('x',duzina/2).attr('y',10)
-                         .attr('text-anchor','middle')
-                         .attr('font-size',textSize).attr('font-family','Poppins')
-                         .attr('fill','white').text(d.id);
+                            // add rectangle
+                            d3.select("g#"+d.id).append('rect').
+                                attr('x',0).attr('y',0).attr('width',width).attr('height',high)
+                                .attr('fill','#003B73');
 
-                        //Ubacivanje razdelnika
-                         d3.select("g#"+d.id).append('line').
-                         attr('x1',0).attr('y1',textSize).attr('x2',duzina).attr('y2',textSize)
-                         .attr('stroke','gray').attr('stroke-width',2);
+                            // add id
+                            d3.select("g#"+d.id).append('text').attr('x',width/2).attr('y',10)
+                            .attr('text-anchor','middle')
+                            .attr('font-size',textSize).attr('font-family','Poppins')
+                            .attr('fill','white').text(d.id);
 
-                        // Ubacivanje teksta za kategorije
+                            // add divider
+                            d3.select("g#"+d.id).append('line').
+                            attr('x1',0).attr('y1',textSize).attr('x2',width).attr('y2',textSize)
+                            .attr('stroke','gray').attr('stroke-width',2);
+
+                            // add attributes
                             for(var i=0;i<attributesNum;i++)
-                         {
-                            
-                             d3.select("g#"+d.id).append('text').attr('x',0).attr('y',20+i*textSize)
-                            .attr('text-anchor','start')
-                             .attr('font-size',textSize).attr('font-family','Poppins')
-                             .attr('fill','white').text(d.attributes[i]);
-
+                            {
+                                d3.select("g#"+d.id).append('text').attr('x',0).attr('y',20+i*textSize)
+                                .attr('text-anchor','start')
+                                .attr('font-size',textSize).attr('font-family','Poppins')
+                                .attr('fill','white').text(d.attributes[i]);
                             }
                         }
 
                         function tick(e) {
-
                             node.attr("transform", function(d) {return "translate(" + d.x + "," + d.y + ")";})
                                 .call(force.drag);
 
